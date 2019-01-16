3d82b855bb44c7dc5ee0ce9ecefd3b66ab1f1f11
seven <819602060@qq.com>
2019-01-16 17:25:41 +0800

    整合mybaits生成器

---
+++ src/main/java/person/seven/mapper/UserMapper.java
@@ -0,0 +1,30 @@
+package person.seven.mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import person.seven.entity.User;
+import person.seven.entity.UserExample;
+
+public interface UserMapper {
+    long countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
